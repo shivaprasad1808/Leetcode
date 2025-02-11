7d1f03fe0c3dfd6012c89027f2f96d53c46e6f54
shivaprasad1808 <42368144+shivaprasad1808@users.noreply.github.com>
2025-02-11 19:09:07 +0530

    Time: 339 ms (88.00%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
 select id, movie, description, rating
 from Cinema
-where id%2 <>0 and description <> 'boring'
+where id%2 =1  and description <> 'boring'
 order by rating desc
