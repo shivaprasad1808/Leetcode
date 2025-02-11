f80b8b9cde58fa4fe794c6cb9a9a98d2da029f24
shivaprasad1808 <42368144+shivaprasad1808@users.noreply.github.com>
2025-02-11 13:42:19 +0530

    Time: 1334 ms (92.74%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,8 +1,7 @@
 /* Write your T-SQL query statement below */
-
-select customer_id, count(customer_id) as count_no_trans
+select customer_id, count(visit_id) as count_no_trans
 from  Visits
 where visit_id
 NOT IN
 (select visit_id from Transactions)
-group by customer_id
+group by customer_id 
