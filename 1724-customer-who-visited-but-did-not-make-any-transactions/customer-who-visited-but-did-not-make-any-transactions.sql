f9af0738500c2da62e708c4ef68b717d97d91153
shivaprasad1808 <42368144+shivaprasad1808@users.noreply.github.com>
2025-02-11 11:47:07 +0530

    Time: 3478 ms (34.35%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+
+select customer_id, count(customer_id) as count_no_trans
+from  Visits
+where visit_id
+NOT IN
+(select visit_id from Transactions)
+group by customer_id
