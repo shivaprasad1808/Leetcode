01c7dbebb9bfac5d45a3aded8071530755629580
shivaprasad1808 <42368144+shivaprasad1808@users.noreply.github.com>
2025-02-12 15:13:18 +0530

    Time: 2346 ms (54.13%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,6 @@
+/* Write your T-SQL query statement below */
+
+select activity_date as day,   count(distinct user_id) as  active_users
+from Activity
+where activity_date between dateadd(day, -29, '2019-07-27') and  '2019-07-27'
+group by activity_date;
