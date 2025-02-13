01a9dcf1c67c0e8671ebe1c860390105640ba2a4
shivaprasad1808 <42368144+shivaprasad1808@users.noreply.github.com>
2025-02-13 09:43:48 +0530

    Time: 579 ms (72.93%) | Memory: 0B (100.00%) - LeetSync

---
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+with CTE as (
+select num from MyNumbers
+group by num
+having count(num)=1)
+SELECT ISNULL(
+(SELECT TOP 1 num FROM CTE ORDER BY num DESC), 
+NULL) AS num;
