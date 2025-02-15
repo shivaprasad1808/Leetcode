e787444b33483488c8e9ce507e7e1812dad3f6ac
shivaprasad1808 <42368144+shivaprasad1808@users.noreply.github.com>
2025-02-15 20:11:04 +0530

    Update product-sales-analysis-iii.sql

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,3 +1,5 @@
+/*Solution 1*/
+
 select a.product_id, a.first_year, s.quantity, s.price
 from (
 select product_id, min(year) as first_year
@@ -7,3 +9,11 @@ group by product_id
 join Sales s
 on a.product_id= s.product_id
 and a.first_year= s.year
+
+/*Solution 2*/
+
+ with CTE as (
+select product_id, year , quantity, price, row_number() over(partition by product_id order by year asc) as rn  
+from Sales )
+select product_id, year as first_year, quantity, price
+from CTE where rn=1
