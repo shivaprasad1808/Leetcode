6696087422c294f1ac178d48c56b1df283ee7778
shivaprasad1808 <42368144+shivaprasad1808@users.noreply.github.com>
2025-02-15 18:48:39 +0530

    Time: 424 ms (76.54%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,10 +1,6 @@
-/* Write your T-SQL query statement below */
-with CTE as (
-select a.machine_id, 
- b.timestamp - a.timestamp as processing_time
- from Activity a join Activity b
- on a.machine_id = b.machine_id
- and a.process_id = b.process_id
- and b.timestamp >a. timestamp)
- select machine_id, round(avg(processing_time),3) as processing_time
- from CTE group by machine_id;
+with CTE as ( 
+select machine_id, timestamp-lag(timestamp) over(partition by machine_id, process_id order by  timestamp) as processing_time
+from Activity )
+select machine_id, round(avg(processing_time),3) as processing_time
+from CTE
+group by machine_id
